87c2dc8852826428579a109bc02f496e8cb4cd97
Mehmood Ahmad <mehmoodahmadq@gmail.com>
2023-08-05 23:47:09 +0500

    Cron job updated

--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { StationService } from './station.service';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
+  constructor(private readonly stationService: StationService) {}
+
   @Cron('0 * * * *')
-  handleCron() {
+  async handleCron() {
     this.logger.log('Called');
+    await this.stationService.fetchData();
   }
 }
